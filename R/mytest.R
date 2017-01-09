1a967e072708321134ed0f6f05516c392e3b6c76
jmcurran <j.curran@auckland.ac.nz>
2017-01-09 16:19:50 +1300

    Added a large result set so that the programme doesn't need to be constantly re-run to get some results to work with

--- R/mytest.R
+++ R/mytest.R
@@ -1,7 +1,7 @@
 mytest = function(){
   bd = readData(system.file("extdata", "data_BB04.inp", package = "rbayesfst"))
   bf = init(bd)
-  setRunParams(bf, numOut = 21)
+  setRunParams(bf, numOut = 201)
   r = sample(bf, seed = 123)
   plot(r$beta)
 }
